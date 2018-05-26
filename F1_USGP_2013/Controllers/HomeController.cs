c50beaeade5b81a27fab2f3b1bb5df80098fc6cb
isaacrael <grael@comcast.net>
2018-05-25 20:19:16 -0600

    added image to About page, added line breaks and removed message from Contact page, added image to images folder

--- F1_USGP_2013/Controllers/HomeController.cs
+++ F1_USGP_2013/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace F1_USGP_2013.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "";
 
             return View();
         }
