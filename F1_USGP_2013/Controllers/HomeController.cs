bd07edb0db630c9c879753752d25ca9e30252fbe
isaacrael <grael@comcast.net>
2018-06-07 15:01:16 -0600

    added System.Web namespace, and commented out social media share code in contact page

--- F1_USGP_2013/Controllers/HomeController.cs
+++ F1_USGP_2013/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using F1_USGP_2013.Models;
 
